2463c13d025cc23d25d833f25d68f5b12b464e39
Nicolas <1075039737@qq.com>
2022-07-05 13:34:15 +0800

    创建layout

---
+++ website/src/layout/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <n-layout>
+        <n-layout-header>颐和园路</n-layout-header>
+        <n-layout has-sider>
+            <n-layout-sider content-style="padding: 24px;">海淀桥</n-layout-sider>
+            <n-layout-content content-style="padding: 24px;">平山道</n-layout-content>
+        </n-layout>
+    </n-layout>
+</template>
+<script setup lang="ts">
+import { NLayout, NLayoutHeader, NLayoutContent, NLayoutSider } from "naive-ui"
+</script>
+<style scoped>
+.n-layout-header,
+.n-layout-footer {
+    background: rgba(128, 128, 128, 0.2);
+    padding: 24px;
+}
+
+.n-layout-sider {
+    background: rgba(128, 128, 128, 0.3);
+}
+
+.n-layout-content {
+    background: rgba(128, 128, 128, 0.4);
+}
+</style>
--- website/vue.config.js
+++ website/vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service")
+const { resolve } = require("path")
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+    transpileDependencies: true,
+    chainWebpack: config => {
+        config.resolve.alias.set("@", resolve("src"))
+    },
+})
