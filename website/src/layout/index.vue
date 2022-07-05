6250da701d25c5019493cc163bd194ee6b36b49a
Nicolas <1075039737@qq.com>
2022-07-05 15:28:01 +0800

    初步搭建layout

--- website/src/layout/index.vue
+++ website/src/layout/index.vue
@@ -1,14 +1,89 @@
 <template>
-    <n-layout>
-        <n-layout-header>颐和园路</n-layout-header>
-        <n-layout has-sider>
-            <n-layout-sider content-style="padding: 24px;">海淀桥</n-layout-sider>
-            <n-layout-content content-style="padding: 24px;">平山道</n-layout-content>
-        </n-layout>
-    </n-layout>
+    <NLayout>
+        <NLayoutHeader>颐和园路</NLayoutHeader>
+        <NLayout has-sider>
+            <NLayoutSider
+                bordered
+                collapse-mode="width"
+                :collapsed-width="64"
+                :width="240"
+                :collapsed="collapsed"
+                show-trigger
+                @collapse="collapsed = true"
+                @expand="collapsed = false"
+            >
+                <NMenu
+                    :collapsed="collapsed"
+                    :collapsed-width="64"
+                    :collapsed-icon-size="22"
+                    :options="menuOptions"
+                    :render-label="renderMenuLabel"
+                    :render-icon="renderMenuIcon"
+                    :expand-icon="expandIcon"
+                    :active-key="activeKey"
+                />
+            </NLayoutSider>
+            <NLayoutContent NLayoutContent content-style="padding: 24px;">平山道</NLayoutContent>
+        </NLayout>
+    </NLayout>
 </template>
 <script setup lang="ts">
-import { NLayout, NLayoutHeader, NLayoutContent, NLayoutSider } from "naive-ui"
+import {
+    NLayout,
+    NLayoutHeader,
+    NLayoutContent,
+    NLayoutSider,
+    MenuOption,
+    NIcon,
+    NMenu,
+} from "naive-ui"
+import { ref, Component, h } from "vue"
+import {
+    BookOutline as BookIcon,
+    PersonOutline as PersonIcon,
+    WineOutline as WineIcon,
+} from "@vicons/ionicons5"
+let collapsed = ref<boolean>(false)
+let activeKey = ref<string | null>("home")
+function renderIcon(icon: Component) {
+    return () => h(NIcon, null, { default: () => h(icon) })
+}
+const renderMenuLabel = (option: MenuOption) => {
+    if ("href" in option) {
+        return h("a", { href: option.href, target: "_blank" }, option.label as string)
+    }
+    return option.label as string
+}
+const renderMenuIcon = (option: MenuOption) => {
+    // 渲染图标占位符以保持缩进
+    if (option.key === "sheep-man") return true
+    // 返回 falsy 值，不再渲染图标及占位符
+    if (option.key === "food") return null
+    return h(NIcon, null, { default: () => h(BookIcon) })
+}
+const expandIcon = () => {
+    return h(NIcon, null, { default: () => h(PersonIcon) })
+}
+let menuOptions: MenuOption[] = [
+    {
+        label: "首页",
+        path: "/",
+        key: "home",
+        icon: renderIcon(BookIcon),
+    },
+    {
+        label: "文章",
+        path: "/article",
+        key: "article",
+        icon: renderIcon(PersonIcon),
+    },
+    {
+        label: "关于",
+        key: "about",
+        path: "/about",
+        icon: renderIcon(WineIcon),
+    },
+]
 </script>
 <style scoped>
 .n-layout-header,
