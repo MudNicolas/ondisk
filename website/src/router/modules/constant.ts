b55c01ae8591848521fcdf3733581a9644962143
Nicolas <1075039737@qq.com>
2022-07-06 15:26:29 +0800

    完善route与constantRoutes

--- website/src/router/modules/constant.ts
+++ website/src/router/modules/constant.ts
@@ -1,12 +1,40 @@
 import { route } from "@/type/index"
+import Layout from "@/layout/index.vue"
 const constantRoutes: route[] = [
     {
         key: "login",
         path: "/login",
-        component: () => import("@/views/login.vue"),
+        component: () => import("@/views/login/index.vue"),
         meta: {
             title: "登录",
             icon: "LoginOutline",
+        },
+    },
+    {
+        path: "/redirect",
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: "/redirect/:path(.*)",
+                component: () => import("@/views/redirect/index.vue"),
+            },
+        ],
+    },
+    {
+        path: "/401",
+        component: () => import("@/views/error-page/401.vue"),
+        hidden: true,
+        meta: {
+            title: "401",
+        },
+    },
+    {
+        path: "/404",
+        component: () => import("@/views/error-page/404.vue"),
+        hidden: true,
+        meta: {
+            title: "404",
         },
     },
 ]
--- website/src/type/index.d.ts
+++ website/src/type/index.d.ts
@@ -1,12 +1,19 @@
 import { Component } from "vue"
 
-export declare type route = {
-    component: Component
+export declare type route = routeRaw | routeWithChildren
+
+export declare type routeRaw = {
     path: string
     key: string | number
-    meta: {
+    component?: Component
+    hidden?: boolean
+    meta?: {
         title: string
         icon?: string
         role?: string
     }
+}
+
+export declare type routeWithChildren = route & {
+    children: route
 }
