3097980ecb20c163c61e1ddfdac1065a5936a39e
Nicolas <1075039737@qq.com>
2022-07-07 14:40:47 +0800

    区分router与sidebar menu

--- website/src/router/modules/constant.ts
+++ website/src/router/modules/constant.ts
@@ -1,8 +1,8 @@
 import { route } from "@/type/index"
+import { RouteRecordRaw } from "vue-router"
 import Layout from "@/layout/index.vue"
-const constantRoutes: route[] = [
+const constantRoutes: RouteRecordRaw[] = [
     {
-        key: "login",
         path: "/login",
         component: () => import("@/views/login/index.vue"),
         meta: {
@@ -11,18 +11,14 @@ const constantRoutes: route[] = [
         },
     },
     {
-        key: "/",
         path: "/",
         redirect: "/drive",
     },
     {
         path: "/redirect",
-        key: "redirectRoot",
         component: Layout,
-        hidden: true,
         children: [
             {
-                key: "redirectBody",
                 path: "/redirect/:path(.*)",
                 component: () => import("@/views/redirect/index.vue"),
             },
@@ -30,18 +26,16 @@ const constantRoutes: route[] = [
     },
     {
         path: "/401",
-        key: "401",
+
         component: () => import("@/views/error-page/401.vue"),
-        hidden: true,
         meta: {
             title: "401",
         },
     },
     {
         path: "/404",
-        key: "404",
+
         component: () => import("@/views/error-page/404.vue"),
-        hidden: true,
         meta: {
             title: "404",
         },
